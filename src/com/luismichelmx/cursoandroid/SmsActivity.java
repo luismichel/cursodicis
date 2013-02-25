43bf86500b1be5404b6ebc32738c50dc5224f1b2
Luis Michel <darkeva02@M11xR2Ubuntu.(none)>
2013-02-25 15:36:16 -0600

    Se actualizó el código hasta el ejemplo de la notificación en la status bar

--- src/com/luismichelmx/cursoandroid/SmsActivity.java
+++ src/com/luismichelmx/cursoandroid/SmsActivity.java
@@ -2,6 +2,15 @@ package com.luismichelmx.cursoandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +18,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SmsActivity extends Activity {
 	
@@ -16,11 +26,17 @@ public class SmsActivity extends Activity {
 	private EditText edit_text;
 	private Button button_send;
 	private String appTag = "curso android";
+	private Context appContext;
+	private Activity smsActivity;
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sms);
+		
+		appContext = this.getApplicationContext();
+		smsActivity = this;
 		
 		edit_phone = (EditText)findViewById(R.id.et_phone);
 		edit_text = (EditText)findViewById(R.id.et_text);
@@ -36,9 +52,18 @@ public class SmsActivity extends Activity {
 				String num = edit_phone.getText().toString();
 				String msg = edit_text.getText().toString();
 				
-				SmsManager sms = SmsManager.getDefault();
-				sms.sendTextMessage(num, null, msg, null, null);
+				if( num.length() != 10 )
+				{
+					Toast.makeText(appContext, "Número de teléfono tiene que ser de 10 dígitos", Toast.LENGTH_LONG).show();
+					
+				}
+				else
+				{
+					sendSMS(num, msg);
+				}
 				
+				//finish() termina la activity en la que fue llamada
+				finish();
 			}
 			
 		});
@@ -46,6 +71,74 @@ public class SmsActivity extends Activity {
 		
 		
 	}
+	
+	//Función para mandar un mensaje SMS
+	private void sendSMS(String phoneNumber, String message)
+	{
+
+		String SENT = "SMS_SENT";
+		
+		PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+		
+		this.registerReceiver(new BroadcastReceiver(){
+
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				// TODO Auto-generated method stub
+				
+				NotificationManager notifManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+				int icon = R.drawable.ic_stat_sms;
+				String ticker;
+				String contentTitle;
+				String contentText;
+				long when = System.currentTimeMillis();
+				Notification notification;
+				
+				switch( getResultCode() )
+				{
+				case Activity.RESULT_OK:
+					Toast.makeText(appContext, "Se envió correctamente el mensaje", Toast.LENGTH_LONG).show();
+					
+					contentTitle = "Curso Android";
+					contentText = "Se envió correctamente el mensaje";
+					ticker = "SMS: Éxito";
+					
+					//Usamos las funciones absoletas para compatiblidad con versiones menores a 3.0 de Android
+					notification = new Notification(icon,ticker,when);
+					
+					Intent notificationIntent = new Intent(smsActivity, MainActivity.class);
+					PendingIntent contentIntent = PendingIntent.getActivity(smsActivity, 0, notificationIntent, 0);
+					notification.setLatestEventInfo(appContext, contentTitle, contentText, contentIntent);
+					
+					notifManager.notify(1, notification);
+					break;
+					
+				//Si quieren probar estos casos, intenten mandar el mensaje a un número que no existe, o pongan el modo avión (Esto si tienen un dispositivo físico)
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(appContext, "Error: falla genérica", Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_NO_SERVICE:
+				Toast.makeText(appContext, "Error: No hay servicio", Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_NULL_PDU:
+					Toast.makeText(appContext, "Error: null pdu", Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_RADIO_OFF:
+					Toast.makeText(appContext, "Error: Radio apagado", Toast.LENGTH_LONG).show();
+					break;
+				}
+			}
+			
+		}, new IntentFilter(SENT));
+			
+		
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(phoneNumber, null, message, sentPI, null);
+		
+			
+		
+	} 
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
